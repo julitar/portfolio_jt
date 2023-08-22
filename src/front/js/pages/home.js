2c05edd74d301b66b9974f6b53bb0a4bc4976b30
julitar <126813769+julitar@users.noreply.github.com>
2023-08-22 14:41:33 +0000

    home buttons and links

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,8 +8,8 @@ export const Home = () => {
 
 	return (
 		<div className="container my-5">
-			<div className="row w-75 m-auto">
-				<div className="col-lg-5 col-md-12 order-lg-2 text-center">
+			<div className="row w-75 m-auto d-flex">
+				<div className="col-lg-5 col-md-12 order-lg-2 justify-content-center">
 					<div className="img-container">
 						<img src={profileimg} alt="Imagen Circular" class="img-fluid custom-circle-img"/>
 					</div>
@@ -20,12 +20,21 @@ export const Home = () => {
 					<p className="bio">
 						My passion lies in building empathy, prototyping concepts, and iterating for the solution.
 						With a decade of experience in education, I've seamlessly transitioned to leading transformative
-						digital projects. Recent certifications in UX design and programming further enhance my ability
-						to create innovative and user-centered solutions.
+						digital projects. Recent certifications in <strong>UX design</strong> and <strong>programming</strong> further enhance my ability
+						to create <strong>innovative and user-centered</strong> solutions.
 					</p>
 				</div>
-				<div className="row botones">
-
+				<div className="row botones order-lg-3">
+					<div className="col-12 my-1">
+					<a href="https://www.linkedin.com/in/julia-tartaglia-71766a68/" target="_blank" className="button-link">
+						<button className="btn my-button">LinkedIn Profile</button>
+					</a>
+					</div>
+					<div className="col-12 my-1">
+						<a href="https://drive.google.com/file/d/1MGKnFmOZswinky-BslMmZn0toYB95Yd2/view?usp=sharing" target="_blank" className="button-link">
+							<button className="btn my-button">Download CV</button>
+						</a>
+					</div>
 				</div>
   			</div>
 		</div>
