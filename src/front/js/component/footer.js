7c0d2406b6778d2be0027ea600b1274965f4c536
julitar <126813769+julitar@users.noreply.github.com>
2023-08-22 14:14:46 +0000

    home

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import "../../styles/home.css";
+
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+		<div className="row">
+			<div className="col-12 border-bottom">
+				<h4 className="footer-subtitle">Get in touch!</h4>
+			</div>
+			<div className="col-12 mt-3">
+				<p className="footer-text">tartaglia.julia@gmail.com</p>
+				<a className="footer-a" href="https://www.linkedin.com/in/julia-tartaglia-71766a68/" target="_blank"><p className="footer-text">LinkedIn</p></a>
+			</div>
+		</div>
 	</footer>
 );
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,34 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import profileimg from "../../img/profileimg.jpg";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+		<div className="container my-5">
+			<div className="row w-75 m-auto">
+				<div className="col-lg-5 col-md-12 order-lg-2 text-center">
+					<div className="img-container">
+						<img src={profileimg} alt="Imagen Circular" class="img-fluid custom-circle-img"/>
+					</div>
+				</div>
+				<div className="col-lg-7 col-md-12 order-lg-1">
+					<h1 className="mt-5 name">Julia</h1>
+					<h2 className="mb-5 lastname">Tartaglia</h2>
+					<p className="bio">
+						My passion lies in building empathy, prototyping concepts, and iterating for the solution.
+						With a decade of experience in education, I've seamlessly transitioned to leading transformative
+						digital projects. Recent certifications in UX design and programming further enhance my ability
+						to create innovative and user-centered solutions.
+					</p>
+				</div>
+				<div className="row botones">
+
+				</div>
+  			</div>
 		</div>
+
 	);
 };
